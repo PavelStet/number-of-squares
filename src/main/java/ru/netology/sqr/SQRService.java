8f4779803f0b1bcf133ce0cd963bd6acae052726
Павел Стецурин <97972115+PavelStet@users.noreply.github.com>
2022-04-05 18:53:17 +0200

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -5,7 +5,7 @@ public class SQRService {
     public int sqrService(int valueLimitsMin, int valueLimitsMax) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
-            if (valueLimitsMin < i * i && i * i < valueLimitsMax) {
+            if (valueLimitsMin <= i * i && i * i <= valueLimitsMax) {
                 counter++;
             }
         }
